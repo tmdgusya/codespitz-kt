40556e37b244ed46da06dfd65ea55dc33f5adcb3
tmdgusya <tmdgusya@naver.com>
2022-06-13 22:58:42 +0900

    [Week02] Stringify 1차 코드

---
+++ src/main/kotlin/parser/Parser.kt
@@ -0,0 +1,24 @@
+package parser
+
+import kotlin.reflect.KProperty
+
+fun <T : Any> stringify(target: T): String {
+    val builder = buildString {
+        target::class.members
+            .filterIsInstance<KProperty<*>>()
+            .joinTo(buffer = this, separator = ", ", prefix = "{", postfix = "}") {
+                val value = it.getter.call(target) // call 은 java reflection 의 invoke 를 생각하면 됨.
+                "${jsonString(it.name)} : ${if (value is String) jsonString(value) else value}"
+            }
+    }
+
+    return builder
+}
+
+private fun jsonString(v: String) = """"${v.replace("\"", "\\\"")}""""
+
+class Json0(val a: Int, val b: String)
+
+fun main() {
+    println(stringify(Json0(0, "roach")))
+}
