b9c00cb416696384494147d12a2955506dc74f33
tmdgusya <tmdgusya@naver.com>
2022-06-13 23:33:17 +0900

    [Week02] Stringify 4차 코드
    Recursive Object

--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -2,37 +2,38 @@ package parser
 
 import kotlin.reflect.KProperty
 
-fun <T : Any> stringify(target: T): String {
-    return jsonObject(target)
-}
-
 fun <T : Any> jsonObject(target: T): String {
     val builder = buildString {
         target::class.members
             .filterIsInstance<KProperty<*>>()
             .joinTo(buffer = this, separator = ", ", prefix = "{", postfix = "}") {
-                val value = it.getter.call(target) // call 은 java reflection 의 invoke 를 생각하면 됨.
-                "${jsonString(it.name)} : ${when (value) {
-                    null -> "null"
-                    is String -> jsonString(value)
-                    is Boolean, is Number -> value.toString()
-                    is List<*> -> jsonList(value)
-                    else -> jsonObject(value)
-                }}"
+                "${stringify(it.name)} : ${stringify(it.getter.call(target))}"
             }
     }
 
     return builder
 }
 
+private fun stringify(value: Any?) = when (value) {
+    null -> "null"
+    is String -> jsonString(value)
+    is Boolean, is Number -> value.toString()
+    is List<*> -> jsonList(value)
+    else -> jsonObject(value)
+}
+
 fun jsonList(target: List<*>): String {
-    return ""
+    val builder = buildString {
+        target.joinTo(buffer = this, separator = ", ", prefix = "[", postfix = "]", transform = ::stringify)
+    }
+
+    return builder
 }
 
 private fun jsonString(v: String) = """"${v.replace("\"", "\\\"")}""""
 
-class Json0(val a: Int, val b: String)
+class Json0(val a: Int, val b: String, val c: List<Int>)
 
 fun main() {
-    println(stringify(Json0(0, "roach")))
+    println(stringify(Json0(0, "roach", listOf(1, 2, 3))))
 }
